a38970567a89dbeb2b909fe70077163ec6823233
robin <robjnz@outlook.com>
2020-07-21 08:08:26 +0100

    update date

--- src/components/Home/on.vue
+++ src/components/Home/on.vue
@@ -4,7 +4,7 @@
       <span style="font-size: 1em;">
         <i class="far fa-calendar-alt"></i>
       </span>
-      Monday 20 July 2020
+      Tuesday 21 July 2020
     </h3>
     <h1 class="text-red-500 text-2xl">
       <span class="text-pink-300" style="font-size: 1em;">
