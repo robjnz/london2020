3a523a80c6aa8d2a9a7163aa52691f568311e4c3
robin <robjnz@outlook.com>
2020-08-10 17:12:10 +0100

    update form

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,24 +1,21 @@
 <template>
   <Layout>
 <section class="container m-auto ml-8 mt-16 text-4xl text-blue-300">
-<form name="contact" method="POST" data-netlify="true">
-  <p>
+
+
+<form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
+     <p>
     <label>Your Name: <input class="text-black bg- bg-blue-200" type="text" name="name" /></label>   
   </p>
   <br />
   <p>
-    <label>Your Email: <input class="text-black bg- bg-blue-200" type="email" name="email" /></label>
+    <label>Email: <input class="text-black bg- bg-blue-200" type="text" name="name" /></label>
   </p>
- <br />
+  <br />
   <p>
-    <label>Message:      <textarea class="text-black bg- bg-blue-200" name="message"></textarea></label>
+    <label>Message: <textarea class="text-black bg- bg-blue-200" name="message"></textarea></label>
   </p>
-<br />
-<div class="field">
-<div 
-data-netlify-recaptcha="true">
-</div>
-</div>
+  <div data-netlify-recaptcha="true"></div>
   <p>
     <button type="submit">Send</button>
   </p>
