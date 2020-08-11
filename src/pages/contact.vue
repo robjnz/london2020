3e1fd8e263d3b776d3da1a38d2b813953f042c00
robin <robjnz@outlook.com>
2020-08-11 07:21:38 +0100

    update form

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,65 +1,38 @@
 <template>
-<Layout>
-<section>
-  <br />
-  <form 
-  name="contact" 
-  method="POST" 
-  data-netlify="true"
-  data-netlify-honeypot="bot-field"
-  >
-  <p>
-    <label>Name: <input type="text" name="name" /></label>   
-  </p>
-  <br />
-  <p>
-    <label>Email: <input type="email" name="email" /></label>
-  </p>
-  <br />
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <br />
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-
-
-
-<form 
-method="POST"  
-name="contact" data-netlify="true"  
-data-netlify-honeypot="bot-field" 
-enctype="application/x-www-form-urlencoded"  
->
-            <input type="hidden" name="form-name" value="contact" />
-            <div className="field" >
-                <label>First Name</label>
-                <input type="text" name="firstName" id="firstName" required />
-            </div>
-
-            <br />
-            <div className="field">
-                <label>Last Name</label>
-                <input type="text" name="lastName" id="lastName"  required />
-            </div>
-
-            <br />
-            <div className="field">
-                <label>Email Address</label>
-                <input type="email" name="portfolioemail" id="email"   />
-          </div>
-          <br />
-          
-          <div className="field">
-             <label>Message</label>
-            <textarea type="message" name="portfoliomessage" id="message"/>
-         </div>
-            <button type="submit">Send</button>
-         </form>
-<br /><br />
-</section>
-</Layout>
-  
+  <form
+    name="contact"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+    >
+    <input type="hidden" name="form-name" value="ask-question" />
+    <label v-for="(panelist, index) in panelists" :key="index">
+      <input
+        type="radio"
+        name="panelist"
+        :value="panelist"
+        @input="ev => updatePanelist"
+        :checked="panelist === currentPanelist"
+      />
+      <span>{{ panelist }}</span>
+    </label>
+    ...
+    <button>Submit</button>
+  </form>
 </template>
+<script>
+export default {
+  name: "QAForm",
+  methods: {
+    updatePanelist (ev) {
+      this.currentPanelist = ev.target.value
+    }
+  },
+  data () {
+    return {
+      panelists: ['Evan You', 'Chris Fritz'],
+      currentPanelist: 'Evan You'
+    }
+  }
+}
+</script>
