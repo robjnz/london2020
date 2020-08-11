2361f1388c56e9166547f2be5cde72669d03058e
robin <robjnz@outlook.com>
2020-08-11 07:05:31 +0100

    update form

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -24,6 +24,40 @@
     <button type="submit">Send</button>
   </p>
 </form>
+
+
+
+<form 
+method="POST"  
+name="contact" data-netlify="true"  
+data-netlify-honeypot="bot-field" 
+enctype="application/x-www-form-urlencoded"  
+>
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="field" >
+                <label>First Name</label>
+                <input type="text" name="firstName" id="firstName" required />
+            </div>
+
+            <br />
+            <div className="field">
+                <label>Last Name</label>
+                <input type="text" name="lastName" id="lastName"  required />
+            </div>
+
+            <br />
+            <div className="field">
+                <label>Email Address</label>
+                <input type="email" name="portfolioemail" id="email"   />
+          </div>
+          <br />
+          
+          <div className="field">
+             <label>Message</label>
+            <textarea type="message" name="portfoliomessage" id="message"/>
+         </div>
+            <button type="submit">Send</button>
+         </form>
 <br /><br />
 </section>
 </Layout>
