88f37d3270ea99c84dbab958634eecf2d806fb0a
robin <robjnz@outlook.com>
2020-08-11 06:12:18 +0100

    update date

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,29 +1,25 @@
 <template>
-  <Layout>
-<section class="container m-auto ml-8 mt-16 text-4xl text-blue-300">
-
-
-<form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
-     <p>
-    <label>Name: <input class="text-black bg- bg-blue-200" type="text" name="name" /></label>   
+<Layout>
+<section>
+  <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
   </p>
   <br />
   <p>
-    <label>Email: <input class="text-black bg- bg-blue-200" type="text" name="email" /></label>
+    <label>Your Email: <input type="email" name="email" /></label>
   </p>
   <br />
   <p>
-    <label>Message: <textarea class="text-black bg- bg-blue-200" name="message"></textarea></label>
+    <label>Message: <textarea name="message"></textarea></label>
   </p>
-  <div data-netlify-recaptcha="true"></div>
+  <br />
   <p>
     <button type="submit">Send</button>
   </p>
 </form>
-
-
-
 <br /><br />
 </section>
- </Layout>
+</Layout>
+  
 </template>
