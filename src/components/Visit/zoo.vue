597093cd5b76d9056bf9dd54a48ad653272954e9
robin <robjnz@outlook.com>
2020-09-27 11:52:20 +0100

    update date visit

--- src/components/Visit/zoo.vue
+++ src/components/Visit/zoo.vue
@@ -17,19 +17,18 @@
           <i class="fas fa-landmark"></i>
         </span>
         London Zoo 
-</H2
-      >
+</H2>
+
       <g-link href="https://bit.ly/2xspOPI" target="_blank"
         ><g-image
           class = "w-auto h-auto"
           src   = "https://res.cloudinary.com/dzhbfdfa5/image/upload/c_scale,h_200,w_300/v1585290413/zoo_bpl1vd.jpg"
-          fluid-grow
           alt = "London zoo"
       /></g-link>
       <p class="text-blue-200">
         Regent's Park Outer Circle<br /> London 
         <g-link
-          class  = "text-red-700 hover:text-white"
+          class  = "postcode hover:text-white"
           to     = "https://bit.ly/2wwYm3r"
           target = "_blank"
         >
@@ -37,30 +36,33 @@
         </g-link>
       </p>
 
-       <h5 class="text-2xl text-pink-300"></h5>
-     <h5 class=" mb-4 text-base text-orange-300">
+     <h2 class=" mb-4 text-base text-orange-300">
         <br />
       Adults: £32.50 <br />Concession £29.25 <br />Children: £21.12 
 <br /><br />
    
-      Opening Times: <br />Mon To Sun<br />
+      Opening Times: <br />Monday To Sunday<br />
       Morning 10am To 2pm <br /> Afternoon 2pm To 6pm <br />Required To Book Online<br />
 
       
 
-      </h5>
+      </h2>
       
-       <h5 class=" mb-4 text-2xl text-green-300"></h5>
       <article class="text-blue-200">
   Collection of 673 species<br /> of animals....... 
   <input id="read-more-zoo-1" class="read-more-zoo" type="checkbox">
   <div class="read-more-content">
   mammals birds, reptiles, <br />fish and insects, including <br />a number of rare and endangered species.
   </div>
-  <label class="read-more-zoo-label text-red-700 hover:text-white" for="read-more-zoo-1">Read </label>
+  <label class="read-more-zoo-label postcode hover:text-white" for="read-more-zoo-1">Read </label>
 </article>
+
+<br />
+      <iframe title="london zoo video 30 minute long" width="290" height="200" src="https://www.youtube.com/embed/fygyNQAzRFM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<br />
+
       <g-link
-        class  = "text-red-700 hover:text-white"
+        class  = "postcode hover:text-white"
         to     = "https://bit.ly/2xspOPI"
         target = "_blank"
       >
