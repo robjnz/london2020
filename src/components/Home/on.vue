1b4f47e3c8a326f160f98fd098828cb914cb14d5
robin <robjnz@outlook.com>
2020-10-13 08:04:28 +0100

    update date

--- src/components/Home/on.vue
+++ src/components/Home/on.vue
@@ -5,7 +5,8 @@
       <span style="font-size: 1em;">
         <i class="far fa-calendar-alt"></i>
       </span>
-  Monday 12 October 2020
+  
+  Tuesday 13 October 2020
     </h2>
     <p class="text-red-500 text-2xl">
       <span class="text-blue-100" style="font-size: 1em;">
